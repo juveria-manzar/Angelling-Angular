07dc3301d1aff6eeb064c3d9c36fdd1621679764
Juveria Manzar <juveriadot@gmail.com>
2020-05-31 14:24:59 +0530

    templating

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -7,7 +7,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'AngularProject';
   constructor(){
     console.log('component declared succesffully')
   }
@@ -18,4 +17,13 @@ export class AppComponent implements OnInit{
   private callme():void{
     console.log('calling method on initialization')
   }
+
+  title = 'Artificial Intelligence';
+  details={
+    heading:'What is Artificial Intelligence ?',
+    desc:"In computer science, artificial intelligence (AI), sometimes called machine intelligence, is intelligence demonstrated by machines, in contrast to the natural intelligence displayed by humans. Leading AI textbooks define the field as the study of 'intelligent agents': any device that perceives its environment and takes actions that maximize its chance of successfully achieving its goals. Colloquially, the term 'artificial intelligence' is often used to describe machines (or computers) that mimic 'cognitive' functions that humans associate with the human mind, such as 'learning' and 'problem solving'."
+  }
+
+  fields=['Machine Learning','Deep Learning','Neural Networks']
+
 }
