1144db544424278c0d0c94b7e6178f73ddc3f9ec
Juveria Manzar <juveriadot@gmail.com>
2020-06-01 10:08:33 +0530

    component-in-component

--- src/app/ai/ai.component.ts
+++ src/app/ai/ai.component.ts
@@ -11,5 +11,12 @@ export class AiComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
+  title = 'Artificial Intelligence';
+  details={
+    heading:'What is Artificial Intelligence ?',
+    desc:"In computer science, artificial intelligence (AI), sometimes called machine intelligence, is intelligence demonstrated by machines, in contrast to the natural intelligence displayed by humans. Leading AI textbooks define the field as the study of 'intelligent agents': any device that perceives its environment and takes actions that maximize its chance of successfully achieving its goals. Colloquially, the term 'artificial intelligence' is often used to describe machines (or computers) that mimic 'cognitive' functions that humans associate with the human mind, such as 'learning' and 'problem solving'."
+  }
 
+  fields=['Machine Learning','Deep Learning','Neural Networks']
 }
