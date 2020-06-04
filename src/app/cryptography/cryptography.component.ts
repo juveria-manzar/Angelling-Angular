f36cd2e8902a53eb179a451bda9fcaa6e745c4ce
Juveria Manzar <juveriadot@gmail.com>
2020-06-04 16:36:30 +0530

    routing-prep

--- src/app/cryptography/cryptography.component.ts
+++ src/app/cryptography/cryptography.component.ts
@@ -12,8 +12,8 @@ export class CryptographyComponent implements OnInit {
   ngOnInit(): void {
   }
   data={
-    subject :'Artificial Intelligence',
-    heading:'What is Artificial Intelligence ?',
+    subject :'Cryptography',
+    heading:'What is Cryptography ?',
     desc:"Cryptography or cryptology is the practice and study of techniques for secure communication in the presence of third parties called adversaries. More generally, cryptography is about constructing and analyzing protocols that prevent third parties or the public from reading private messages; various aspects in information security such as data confidentiality, data integrity, authentication, and non-repudiation are central to modern cryptography. Modern cryptography exists at the intersection of the disciplines of mathematics, computer science, electrical engineering, communication science, and physics. Applications of cryptography include electronic commerce, chip-based payment cards, digital currencies, computer passwords, and military communications.",
     related:['Big O Notation','Recursion','Sorting']
   }
--- src/app/internet/internet.component.ts
+++ src/app/internet/internet.component.ts
@@ -12,8 +12,8 @@ export class InternetComponent implements OnInit {
   ngOnInit(): void {
   }
   data={
-    subject :'Artificial Intelligence',
-    heading:'What is Artificial Intelligence ?',
+    subject :'The Internet',
+    heading:'What is Internet?',
     desc:"The Internet is the global system of interconnected computer networks that uses the Internet protocol suite (TCP/IP) to link devices worldwide. It is a network of networks that consists of private, public, academic, business, and government networks of local to global scope, linked by a broad array of electronic, wireless, and optical networking technologies. The Internet carries a vast range of information resources and services, such as the inter-linked hypertext documents and applications of the World Wide Web (WWW), electronic mail, telephony, and file sharing.",
     related:['Big O Notation','Recursion','Sorting']
   }
