6674611bffd5787085f27e658d8a661bf2975781
Juveria Manzar <juveriadot@gmail.com>
2020-06-05 10:50:59 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { AiComponent } from './ai/ai.component';
+import { CryptographyComponent } from './cryptography/cryptography.component';
+import { AlgorithmsComponent } from './algorithms/algorithms.component';
+import { CompressionComponent } from './compression/compression.component';
+import { InternetComponent } from './internet/internet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'cryptography', component:CryptographyComponent,pathMatch:'full'},
+  {path:'', component:HomeComponent,pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
